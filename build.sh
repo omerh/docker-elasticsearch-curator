e0829c66f84f5395764cc25d492e0b892733dcf2
omerh <omerha@protonmail.com>
2018-09-20 11:27:58 +0300

    renamed docker repo

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-docker build --pull --no-cache -t omerha/curator:latest .
-docker push omerha/curator:latest
+docker build --pull --no-cache -t omerha/docker-elasticsearch-curator:latest .
+docker push omerha/docker-elasticsearch-curator:latest
