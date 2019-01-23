b68db735ef62da2fd2ea8c3b603a58d906daf8a5
omerh <omerha@protonmail.com>
2019-01-23 21:06:56 +0200

    making build script more generic

--- build.sh
+++ build.sh
@@ -2,5 +2,11 @@
 
 set -e
 
-docker build --pull --no-cache -t omerha/docker-elasticsearch-curator:latest .
-docker push omerha/docker-elasticsearch-curator:latest
+REPO=$(basename -s .git `git config --get remote.origin.url`)
+REGISTRY='omerha'
+COMMIT=$(git log --format="%H" -n 1)
+
+docker build --pull --no-cache -t $REGISTRY/$REPO:$COMMIT -t $REGISTRY/$REPO:latest .
+
+docker push $REGISTRY/$REPO:latest
+docker push $REGISTRY/$REPO:$COMMIT
